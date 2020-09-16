abc9e965dffc00b78dd569906a8c69a0359765b5
MMendes24 <mmbusiness1@protonmail.com>
2020-09-16 15:44:08 -0400

    First function built and tested.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,18 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('enhancer.js', () => {
+    describe('.repair()', () => {
+        it('can be repaired', function () {
+            const item = {
+                name: "George Washington's Cavalry Saber",
+                durability: "87",
+                enhancement: 0,
+            }
+
+            const repairedItem = enhancer.repair(item)
+
+            expect(repairedItem.durability).toBe(100)
+        })
+    })
+})
