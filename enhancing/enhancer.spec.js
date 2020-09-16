a6fd36c4758c76f60fa706f400114c6eaea3e711
MMendes24 <mmbusiness1@protonmail.com>
2020-09-16 15:58:30 -0400

    Item can be enhanced

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -15,4 +15,17 @@ describe('enhancer.js', () => {
             expect(repairedItem.durability).toBe(100)
         })
     })
+    describe('.success()', () => {
+        it('can be enhanced', function () {
+            const item = {
+                name: "George Washington's Cavalry Saber",
+                durability: "87",
+                enhancement: 0,
+            }
+
+            const enhancedItem = enhancer.success(item)
+
+            expect(enhancedItem.enhancement).toBe(1)
+        })
+    })
 })
